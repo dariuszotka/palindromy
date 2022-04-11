2422a186d2193e0121c0544c362e41a68d2bdd1c
Dorota Pietruszka <do.pietruszka@gmail.com>
2022-04-11 17:53:24 +0200

    merge ideas into one function

--- palindromy.py
+++ palindromy.py
@@ -1,39 +1,20 @@
-#pierwsza metoda
-print("Wpisz słowo:")
-x = input()
-def palindrome_check(x):
-  """checks if given argument is a palindrome
-returns "palindrom"(True) or "nie"(False) based on given argument
-argument: input by user """
-  y = ''.join(reversed(x))
-  while y == x:
-    True
-    print("palidrom")
-    break
-  while y != x:
-    False
-    print("nie")
-    break
-  
-palindrome_check(x)
-
-help(palindrome_check)
-
-#druga metoda
-
-
-print("Wpisz słowo:")
-x = input()
 def palindrome_check(x):
   """checks if given argument is a palindrome
 returns "palindrom"(True) or "nie"(False) based on given argument
 argument: input by user """
   y = ''.join(reversed(x))
   if y == x:
-    print("palindrom")
+    return True
   else:
-    print("nie")
+    return False
+
+print("Wpisz słowo:")
+x = input()
+result = palindrome_check(x)
+if result:
+  print(f"słowo {x} jest palndromem")
+else:
+  print(f"słowo {x} nie jest palindromem")
 
 palindrome_check(x)
 
-help(palindrome_check)
